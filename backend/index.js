2f1e1c24b7c81735c860a61b644597771ece38ad
Namrata <ndubeyansu@gmail.com>
2026-04-28 23:16:34 +0530

    started the server and websockets

---
+++ backend/index.js
@@ -0,0 +1,37 @@
+import http from "http";
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import path from "path";
+import {Server} from "socket.io"
+
+const app=express();
+dotenv.config();
+
+
+
+const server=http.createServer(app)
+const io=new Server(server , {
+  cors: {
+    origin:"http://localhost:5173",
+  },
+})
+
+app.get('/',(req,res)=>{
+  res.send("your server is running successfully")
+})
+
+io.on("connection",(socket)=>{
+  console.log("user connected successfully",socket.id)
+
+  socket.on("disconnect",()=>{
+    console.log("user disconnected !",socket.id)
+  })
+})
+
+const PORT=process.env.PORT || 3000
+server.listen(PORT,()=>{
+  console.log(`server is runing at http://localhost:${PORT}/`)
+});
+
+
